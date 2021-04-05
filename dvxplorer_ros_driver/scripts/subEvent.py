58ba8e2dd13886af81c48c9347d91c4dbc8f25f5
ziimiin14 <erzimin@gmail.com>
2021-04-05 17:03:10 +0800

    first commit with modification

---
+++ dvxplorer_ros_driver/scripts/subEvent.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import rospy
+from dvs_msgs.msg import EventArray
+from std_msgs.msg import Int32
+from rospy.numpy_msg import numpy_msg
+
+def eventsCallback(data):
+    # time_init = data.events[0].ts.nsecs
+    # for event in data.events:
+        # rospy.loginfo('x: %i, y: %i, pol: %i',event.x,event.y,event.polarity)
+        # rospy.loginfo('y: %i',event.y)
+        # rospy.loginfo('ts: %i',event.ts.secs)
+        # rospy.loginfo('pol %i:',event.polarity)
+    rospy.loginfo(data.events)
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard ', data.events)
+
+
+def eventsSizeCallback(data):
+    
+    rospy.loginfo('Event size: %i', data.data)
+
+def subEvent():
+
+
+    rospy.init_node('subEvent', anonymous=True)
+    # rospy.Subscriber('/dvs/events', EventArray,eventsCallback)
+    rospy.Subscriber('/dvs/events_size', Int32, eventsSizeCallback)
+
+
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    print('run main')
+    subEvent()
