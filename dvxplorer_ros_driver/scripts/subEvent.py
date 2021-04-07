5699c4863f6bcb74d1139839652d2bad0b5bcd42
ziimiin14 <erzimin@gmail.com>
2021-04-07 17:10:26 +0800

    added new contiguous messages for event data

--- dvxplorer_ros_driver/scripts/subEvent.py
+++ dvxplorer_ros_driver/scripts/subEvent.py
@@ -2,8 +2,9 @@
 
 import rospy
 from dvs_msgs.msg import EventArray
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Float32MultiArray
 from rospy.numpy_msg import numpy_msg
+import numpy as np
 
 def eventsCallback(data):
     # time_init = data.events[0].ts.nsecs
@@ -15,6 +16,18 @@ def eventsCallback(data):
     rospy.loginfo(data.events)
     # rospy.loginfo(rospy.get_caller_id() + 'I heard ', data.events)
 
+def eventsArrCallback(data):
+    # time_init = data.events[0].ts.nsecs
+    # for event in data.events:
+        # rospy.loginfo('x: %i, y: %i, pol: %i',event.x,event.y,event.polarity)
+        # rospy.loginfo('y: %i',event.y)
+        # rospy.loginfo('ts: %i',event.ts.secs)
+        # rospy.loginfo('pol %i:',event.polarity)
+    event = np.array(data.data)
+    event = event.reshape(-1,4)
+    rospy.loginfo(event.shape)
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard ', data.events)
+
 
 def eventsSizeCallback(data):
     
@@ -24,7 +37,8 @@ def subEvent():
 
 
     rospy.init_node('subEvent', anonymous=True)
-    # rospy.Subscriber('/dvs/events', EventArray,eventsCallback)
+    #rospy.Subscriber('/dvs/events', EventArray,eventsCallback)
+    rospy.Subscriber('/dvs/eventsArr', Float32MultiArray,eventsArrCallback)
     rospy.Subscriber('/dvs/events_size', Int32, eventsSizeCallback)
 
 
