20b53ddfff0a4ed2f64df1075c7b61f4504e22a3
Chung Wah Kit <chungwahkit@gmail.com>
2024-05-29 13:57:28 +0800

    middle timestamp WIP

--- dvxplorer_mc_ceres/src/driver_evk4_lbfgsb.cpp
+++ dvxplorer_mc_ceres/src/driver_evk4_lbfgsb.cpp
@@ -8,33 +8,62 @@ namespace dvxplorer_mc_ceres
 
 	void MyProcessor::eventCD(uint64_t t, uint16_t ex, uint16_t ey, uint8_t)
 	{
+		if (frame_processed_)
+		{
+			if (t < t_new_frame_)
+			{
+				return;
+			}
+			else{
+				frame_processed_=false;
+				reached_first_midpoint_frame_=false;
+				reached_last_midpoint_frame_=false;
+				last_event_idx_==-1;
+			}
+		}
+		else if (reached_first_midpoint_frame_&&!reached_last_midpoint_frame_)
+		{
+			if(t>t_midpoint_){
+				reached_last_midpoint_frame_=true;
+				last_event_idx_=(mc_gr->num_events_+first_midpoint_event_idx_)/2 + mc_gr->max_num_events_/2;
+			}
+		}
+		else if (t_new_frame_ == -1)
+		{
+			t_new_frame_ = t;
+			t_midpoint_=t_new_frame_+0.5e-2;
+		}
+		else if(!reached_first_midpoint_frame_&&t>=t_midpoint_){
+			reached_first_midpoint_frame_=true;
+			first_midpoint_event_idx_=mc_gr_->num_events_;
+		}
 
-		if(t_new_frame_==-1){
-			t_new_frame_=t;
-		}
-		if(t<t_new_frame_){
-			return;
-		}
-		//TODO: equal events before and after middle ts 
-		// mc_gr_->AddData(t,ex-440,ey-160);
+
+		// TODO: equal events before and after middle ts
+		//  mc_gr_->AddData(t,ex-440,ey-160);
 		mc_gr_->AddData(t, ex + x_offset_, ey + y_offset_);
 		// *outfile_<<t<<","<<ex<<","<<ey<<","<<0<<std::endl;
-		if (mc_gr_->ReadyToMC())
+		// if (mc_gr_->ReadyToMC())
+		if (mc_gr_->num_events_==last_event_idx_)
 		{
 			idx++;
 
 			// Initial guess
-			float fx; 
-			try{
-			solver_.minimize(*mc_gr_, rotations_, fx, lb_, ub_);
+			float fx;
+			try
+			{
+				solver_.minimize(*mc_gr_, rotations_, fx, lb_, ub_);
 			}
-			catch (std::exception &e){
-				std::string error_str=e.what();
-				if(error_str=="the line search step became smaller than the minimum value allowed"){
+			catch (std::exception &e)
+			{
+				std::string error_str = e.what();
+				if (error_str == "the line search step became smaller than the minimum value allowed")
+				{
 					ROS_INFO("<min");
 				}
-				else{
-					ROS_INFO("%s",error_str.c_str());
+				else
+				{
+					ROS_INFO("%s", error_str.c_str());
 				}
 			}
 			ROS_INFO("%d iterations", mc_gr_->iterations);
@@ -66,30 +95,32 @@ namespace dvxplorer_mc_ceres
 			tjFree(jpeg_buffer);
 			compensated_image_pub_.publish(compressed);
 			mc_gr_->ClearEvents();
-			t_new_frame_+=1e-2;
+			t_new_frame_ += 1e-2;
+			t_midpoint_ += 1e-2;
+			frame_processed_=true;
 		}
 	}
-	MyProcessor::MyProcessor(ros::NodeHandle &nh) : tjhandle_(makeTjhandleUniquePtr()), nh_(nh),solver_(LBFGSBSolver<float>(param_))
+	MyProcessor::MyProcessor(ros::NodeHandle &nh) : tjhandle_(makeTjhandleUniquePtr()), nh_(nh), solver_(LBFGSBSolver<float>(param_))
 	{
 		// google::InitGoogleLogging("mc_ceres");
 		mc_gr_ = std::make_shared<McGradient>(fx_, fy_, cx_, cy_, height_, width_);
 		mc_gr_->allocate();
 
-		param_.m=10;
+		param_.m = 10;
 		// param_.epsilon = 1e-2;
 		// param.epsilon_rel = 1e-3;
 		param_.max_iterations = 20;
 		param_.delta = 1e-4;
-		
-		rotations_[1]=-20;
-		
+
+		rotations_[1] = -20;
+
 		// c1 aka sufficient decrease
 		// param_.ftol = 1e-6;
 
 		// c2 aka curvature
 		// param_.wolfe = 0.9;
-		param_.min_step=1e-5;
-		param_.max_step=1e3;
+		param_.min_step = 1e-5;
+		param_.max_step = 1e3;
 
 		ns = ros::this_node::getNamespace();
 		if (ns == "/")
@@ -98,7 +129,6 @@ namespace dvxplorer_mc_ceres
 		}
 
 		compensated_image_pub_ = nh_.advertise<sensor_msgs::CompressedImage>(ns + "/comprensated/image/compressed", 10);
-
 	}
 
 	MyProcessor::~MyProcessor()
